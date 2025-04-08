27f8edb06fa3cd18b21213bcb98985b88120ca88
Ad1L2005 <adil.nurgozha@gmail.com>
2025-04-08 12:56:26 +0500

    dobavil classy

---
+++ app/src/main/java/com/example/lab7/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.lab7;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText edUsername, edPassword;
+    private Button btnLogin, btnSignUp;
+    private static final String PREFS_NAME = "UserPrefs";
+
+
+}
---
+++ app/src/main/java/com/example/lab7/SignUpActivity.java
@@ -0,0 +1,11 @@
+package com.example.lab7;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignUpActivity {
+
+    private EditText edUsername, edPassword, edConfirmPassword;
+    private Button btnCreateUser;
+    private static final String PREFS_NAME = "UserPrefs";
+}
